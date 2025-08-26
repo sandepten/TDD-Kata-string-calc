22c04f47d9ac5ef42a7b243ce355171e7b70d0b1
Sandeep Kumar <sandepten@gmail.com>
2025-08-26 16:18:14 +0530

    refactor: handle multiple delimiters

--- src/calculator.ts
+++ src/calculator.ts
@@ -3,7 +3,8 @@ export function add(numbers: string): number {
     return 0;
   }
 
-  const regex = new RegExp(`[,\n]`);
+  const delimiters = [",", "\n"];
+  const regex = new RegExp(`[${delimiters.join("")}]`);
 
   const numArray = numbers.split(regex).map((num) => parseInt(num, 10));
   return numArray.reduce((acc, curr) => acc + curr, 0);
