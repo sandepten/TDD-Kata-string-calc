2e77899d8802cce5de5852239419867eaf49d1e7
Sandeep Kumar <sandepten@gmail.com>
2025-08-26 16:21:33 +0530

    add: handling for custom delimiter provided by input

--- src/calculator.ts
+++ src/calculator.ts
@@ -4,8 +4,14 @@ export function add(numbers: string): number {
   }
 
   const delimiters = [",", "\n"];
-  const regex = new RegExp(`[${delimiters.join("")}]`);
+  if (numbers.startsWith("//")) {
+    const delimiterEndIndex = numbers.indexOf("\n");
+    const customDelimiter = numbers.substring(2, delimiterEndIndex);
+    delimiters.push(customDelimiter);
+    numbers = numbers.substring(delimiterEndIndex + 1);
+  }
 
+  const regex = new RegExp(`[${delimiters.join("")}]`);
   const numArray = numbers.split(regex).map((num) => parseInt(num, 10));
   return numArray.reduce((acc, curr) => acc + curr, 0);
 }
