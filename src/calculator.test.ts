7d88d15848e82b1e89cd944c4f8b4baa63cece9a
Sandeep Kumar <sandepten@gmail.com>
2025-08-26 16:20:57 +0530

    test: support different delimiters (provided by input)

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -17,3 +17,7 @@ test("adds 1,2,3,4,5 to equal 15", () => {
 test("Allow the add method to handle new lines between numbers (instead of commas). ('1\n2,3' should return 6)", () => {
   expect(add("1\n2,3")).toBe(6);
 });
+
+test("Support different delimiters:To change the delimiter, the beginning of the string will contain a separate line that looks like this: '//[delimiter]\\n[numbers…]'. For example, '//;\\n1;2' where the delimiter is ';' should return 3.", () => {
+  expect(add("//;\n1;2")).toBe(3);
+});
