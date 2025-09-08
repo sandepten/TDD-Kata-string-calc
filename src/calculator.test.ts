f1c8895f9237fbf05fdf0b72de03d5b55ac7e308
Sandeep Kumar <sandepten@gmail.com>
2025-09-08 16:37:09 +0530

    refac: solid principle

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -1,44 +1,49 @@
 import { expect, test } from "vitest";
-import { add } from "./calculator";
+import { calculator } from "./calculator";
 
 test("adds 1 + 2 to equal 3", () => {
-  expect(add("1,2")).toBe(3);
+  expect(calculator("1,2")).toBe(3);
 });
 test("adds empty string to equal 0", () => {
-  expect(add("")).toBe(0);
+  expect(calculator("")).toBe(0);
 });
 test("adds 1 to equal 1", () => {
-  expect(add("1")).toBe(1);
+  expect(calculator("1")).toBe(1);
 });
 test("adds 1,2,3,4,5 to equal 15", () => {
-  expect(add("1,2,3,4,5")).toBe(15);
+  expect(calculator("1,2,3,4,5")).toBe(15);
 });
 
 // negative numbers
 test("Calling add with a negative number will throw an exception: 'negative numbers not allowed <negative_number>'.", () => {
-  expect(() => add("-1,2")).toThrow("negative numbers not allowed -1");
+  expect(() => calculator("-1,2")).toThrow("negative numbers not allowed -1");
 });
 test("Calling add with multiple negative numbers will throw an exception: 'negative numbers not allowed <negative_number1>,<negative_number2>'", () => {
-  expect(() => add("2,-4,3,-5")).toThrow("negative numbers not allowed -4,-5");
+  expect(() => calculator("2,-4,3,-5")).toThrow(
+    "negative numbers not allowed -4,-5",
+  );
 });
 
 test("Numbers bigger than 1000 should be ignored, so adding 2 + 1001 = 2", () => {
-  expect(add("2,1001")).toBe(2);
+  expect(calculator("2,1001")).toBe(2);
 });
 
 // delimiter
 test("Allow the add method to handle new lines between numbers (instead of commas). ('1\n2,3' should return 6)", () => {
-  expect(add("1\n2,3")).toBe(6);
+  expect(calculator("1\n2,3")).toBe(6);
 });
 test("Support different delimiters:To change the delimiter, the beginning of the string will contain a separate line that looks like this: '//[delimiter]\\n[numbers…]'. For example, '//;\\n1;2' where the delimiter is ';' should return 3.", () => {
-  expect(add("//;\n1;2")).toBe(3);
+  expect(calculator("//;\n1;2")).toBe(3);
 });
 test("Delimiters can be of any length with the following format: “//[delimiter]\\n” for example: “//[***]\\n1***2***3” should return 6", () => {
-  expect(add("//[***]\n1***2***3")).toBe(6);
+  expect(calculator("//[***]\n1***2***3")).toBe(6);
 });
 test("Allow multiple delimiters like this: “//[delim1][delim2]\\n” for example “//[*][%]\\n1*2%3” should return 6.", () => {
-  expect(add("//[*][%]\n1*2%3")).toBe(6);
+  expect(calculator("//[*][%]\n1*2%3")).toBe(6);
 });
 test("Make sure you can also handle multiple delimiters with length longer than one char. For example: “//[***][%%%]\\n1***2%%%3” should return 6.", () => {
-  expect(add("//[***][%%%]\n1***2%%%3")).toBe(6);
+  expect(calculator("//[***][%%%]\n1***2%%%3")).toBe(6);
+});
+test("If the delimiter is % then multiply the number", () => {
+  expect(calculator("//%\n1%2")).toBe(2);
 });
--- src/calculator.ts
+++ src/calculator.ts
@@ -1,11 +1,34 @@
-export function add(numbers: string): number {
+export function calculator(numbers: string): number {
   if (!numbers) return 0;
 
+  const delimiterResult = delimiterNums(numbers);
+  const numArray = delimiterResult.nums;
+
+  const negativeNumbers = numArray.filter((num) => num < 0);
+  if (negativeNumbers.length) {
+    throw new Error(
+      `negative numbers not allowed ${negativeNumbers.join(",")}`,
+    );
+  }
+  const numArrayLessThan1000 = numArray.filter((num) => num <= 1000);
+
+  return delimiterResult.shouldMultiply
+    ? multiply(numArrayLessThan1000)
+    : add(numArrayLessThan1000);
+}
+
+function delimiterNums(numbers: string): {
+  nums: number[];
+  shouldMultiply: boolean;
+} {
+  let shouldMultiply = false;
   let delimiters = [",", "\n"];
   if (numbers.startsWith("//")) {
     const delimiterEndIndex = numbers.indexOf("\n");
     const delimiterSection = numbers.slice(2, delimiterEndIndex);
-
+    if (delimiterSection.length === 1 && delimiterSection.startsWith("%")) {
+      shouldMultiply = true;
+    }
     if (delimiterSection.startsWith("[") && delimiterSection.endsWith("]")) {
       delimiters = delimiters.concat(
         delimiterSection
@@ -22,13 +45,13 @@ export function add(numbers: string): number {
   const regex = new RegExp(`[${delimiters.join("")}]`);
   const numArray = numbers.split(regex).map(Number);
 
-  const negativeNumbers = numArray.filter((num) => num < 0);
-  if (negativeNumbers.length) {
-    throw new Error(
-      `negative numbers not allowed ${negativeNumbers.join(",")}`,
-    );
-  }
-  return numArray
-    .filter((num) => num <= 1000)
-    .reduce((sum, num) => sum + num, 0);
+  return { nums: numArray, shouldMultiply };
+}
+
+function add(nums: number[]): number {
+  return nums.reduce((a, b) => a + b, 0);
+}
+
+function multiply(nums: number[]): number {
+  return nums.reduce((a, b) => a * b, 1);
 }
